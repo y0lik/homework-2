388ec6a02743389fe32b2ccbf210af2213ca9a50
artemii <mr.y0lik@gmail.com>
2022-03-30 21:28:25 +0300

    V1.0

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-random import
-time import
-a=random.randint(1, 10)
+import random
 print("You have to choose the num from 1 to 10")
+def choose():
+    a = random.randint(1, 10)
+    x=input(Your num: )
+    if x=a:
+        print("Cool, you guess it")
+    else:
+        b=input(Play again? y/n)
+    if b="y":
+        choose()
+    if b="n":
+        break
