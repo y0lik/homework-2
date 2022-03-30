846f2c3a8f400df012433d21a18abf1ca96f6cc7
artemii <mr.y0lik@gmail.com>
2022-03-30 21:56:35 +0300

    V3.0 beta

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import random
 print("You have to choose the num from 1 to 10")
-a = int(random.randint(1, 10))
+
 def choose():
+    a = int(random.randint(1, 10))
     x=input("Your num: ")
-    if x==a:
-        print("Cool, you guess it")
+
+    if x!=a:
+        print("Oh, you lose.")
+        print("Your num was: ", a)
+        b = input("Play again? (y/n) ")
+
     else:
-        b=input("Oh, you lose. Play again? y/n ")
-    if b=="y":
-        return a
+        print("Cool, you guess it")
+        b = input("Play again? (y/n) ")
+
+    if b == "y":
         choose()
-    if b=="n":
+    if b == "n":
         print("okay:(")
 choose()
